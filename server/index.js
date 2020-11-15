56a721b74c541e80fb94342e9e2cbf1074072598
Ki Uk Kang <mike.kang@mail.utoronto.ca>
2020-11-15 02:59:40 -0500

    added in some routes with react router, a few unused imports to clean up later on. I will test wiring up backend now with both class approach and functional programming approach. Perhaps I will make one component use class and use react-redux, and then the other use react toolkit. Afterwards, must spend time re-reading JWT again to figure details.

--- server/index.js
+++ server/index.js
@@ -14,7 +14,11 @@ const server = http.createServer(app);
 app.use(cors());
 app.use(router);
 
-User.findOne({ where: { id: 1} })
-  .then((value) => { console.log(value.get().name); })
+app.get('/user/:id', function(req, res){
+  User.findOne({ where: { id: req.params.id} })
+    .then((value) => { res.send(value.get()); });
+
+});
+
 
 server.listen(process.env.PORT || 5000, () => {console.log('Server has started.')});
